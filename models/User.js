18577af737505eae2f46d2138355bb39d66a5d82
Wyatt Gaines <128557281+Wyatt-design5110@users.noreply.github.com>
2024-11-27 20:11:59 -0600

    User.js

--- models/User.js
+++ models/User.js
@@ -8,22 +8,35 @@ const userSchema = new mongoose.Schema({
     lastName: { type: String, required: true },
     email: { type: String, required: true },
     password: { type: String, required: true },
-    favorites: [{type: mongoose.Schema.Types.ObjectId, ref: 'Place', default: [] }], // array of favorite items from website
-    ceateAt: { type: Date, default: Date.now },
+    favorites: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Place' }] // array of favorite items from website
 });
 
-// Create the User model
-userSchema.methods.addFavorite = async function (placeId) {
-    if (!this.favorite.some(favorite => favorite.toString() === placeId.toString())) {
+userSchema.pre('save', function (next) {
+    this.favorites = [...new Set(this.favorites)];
+    next();
+});
+
+userSchema.methods.addFavorite = function (placeId) {
+    if (!this.favorites.includes(placeId)) {
         this.favorites.push(placeId);
-        await this.save();
     }
+    return this.save();
+};
+userSchema.methods.remove = function (placeId) {
+    this.favorites = this.favorites.filter((id) => id.toString() !== placeId.toString());
+    return this.save();
 };
 
-userSchema.methods.removeFavorite = async function (placeId) {
-    this.favorites = this.favorites.filter((favorite) => favorite.toString() !== placeId.toString());
-    await this.save();
+userSchema.method.isFavorited = function (placeId) {
+    return this.favorites.includes(placeId.toString());
 };
+
+userSchema.statics.findWithFavorites = function (UserID) {
+    return this.findById(userId).populate('favorites');
+};
+
+// Create the User model
+const User = mongoose.model('User', userSchema);
 
 // Export the User model correctly
-module.exports = mongoose.model('User', userSchema);
+module.exports = User;
